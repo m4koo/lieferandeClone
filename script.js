dc4c249a92f142e01d7b854dc9c12b303f563445
m4koo <mahmut.kalem@outlook.de>
2022-12-09 15:14:55 +0100

    added items to array

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let food = ['test'];
-let ingredients = ['test'];
-let price = [4];
+let food = ['Chilli Cheeseburger (scharf)', 'Double Beef-Burger', 'Clasic Hamburger'];
+let ingredients = ['mit 160g Patty, frischen Jalapenos und Chili-Cheesesauce im Brioche', 'mit 2x 160g Patty, Onion-Jam und Cheddar im Brioche', 'mit 160g Patty im Brioche'];
+let price = ['13.90', '18.90', '12.90'];
 
 function render() {
     for (let i = 0; i < food.length; i++) {
@@ -13,11 +13,15 @@ function generateItemsHTML(i){
     let items = document.getElementById('items');
 
     items.innerHTML += `
-        <div class="item-card">
+        <div class="item-card" onclick="addToBasket${i}">
             <h3>${food[i]}</h3>
             <p>${ingredients[i]}</p>
-            <div>${price[i]}</div>
+            <div>${price[i]}$</div>
             <button><img src="img/svg/plus.svg"></button>
         </div>
     `;
+}
+
+function addToBasket(i) {
+    
 }
